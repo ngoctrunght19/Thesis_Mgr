92b37929c3f55e3dfc02407cb7e3b447a2feb9bf
ngoctrunght19 <ngoctrunght19@gmail.com>
2016-12-07 18:14:29 +0700

    trang hoc vien

--- resources/views/hocvien.blade.php
+++ resources/views/hocvien.blade.php
@@ -7,4 +7,76 @@
 @endsection
 
 @section('body')
+<div id="tabs">
+    <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+        <form role="search">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+            </div>
+        </form>
+        <ul class="nav menu">
+            <li>
+                <a href="#tabs-1">
+                    <svg class="glyph stroked notepad "><use xlink:href="#stroked-notepad"/></svg>
+                     Đơn vị
+                </a>
+            </li>
+            <li>
+                <a href="#tabs-2">
+                    <svg class="glyph stroked male user "><use xlink:href="#stroked-male-user"/></svg>
+                     Giảng viên
+                </a>
+            </li>
+            <li>
+                <a href="#tabs-3">
+                    <svg class="glyph stroked open folder"><use xlink:href="#stroked-open-folder"/></svg>
+                     Lĩnh vực
+                </a >
+            </li>
+            <li>
+                <a href="#tabs-4">
+                    <svg class="glyph stroked open folder"><use xlink:href="#stroked-open-folder"/></svg>
+                     Chủ đề nghiên cứu
+                </a>
+            </li>
+            <li>
+                <a href="#tabs-5">
+                    <svg class="glyph stroked star"><use xlink:href="#stroked-star"/></svg>
+                     Đề tài khóa luận
+                </a>
+            </li>
+            <li class="parent">
+                <a data-toggle="collapse" href="#sub-item-1">
+                    <svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg>
+                     Dropdown
+                </a>
+                <ul class="children collapse" id="sub-item-1">
+                    <li>
+                        <a>
+                            Sub Item 1
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Sub Item 2
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Sub Item 3
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+    </div><!--/.sidebar-->
+
+    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main"> 
+        @include('hocvien.donvi')
+        @include('hocvien.giangvien')
+        @include('khoa.qlgv')
+        @include('khoa.qlhv')
+    </div>
+</div>
 @endsection
---
+++ resources/views/hocvien/giangvien.blade.php
@@ -0,0 +1,125 @@
+<div id="tabs-2">
+	<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<h1>Giảng viên</h1>
+					<div class="panel-body">
+						<table data-toggle="table" data-url="tables/data1.json"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
+						    <thead>
+						    <tr>
+						        <th data-sortable="true">Họ và tên</th>
+						        <th data-sortable="true">Đơn vị</th>
+						        <th data-sortable="true">Lĩnh vực</th>
+						        <th data-sortable="true">Chủ đề nghiên cứu</th>
+						    </tr>
+						    </thead>
+						    <tbody>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+								<tr>
+									<td>Nguyễn Bá Tú</td>
+									<td>Khoa công nghệ thông tin</td>
+									<td>Phát triển ứng dụng web</td>
+									<td>web app</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div><!--/.row-->	
+</div>
