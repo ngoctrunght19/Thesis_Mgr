f62dee1f6c104319f20c7cc90872763cb5b8747e
ngoctrunght19 <ngoctrunght19@gmail.com>
2016-12-08 21:09:28 +0700

    modal them giang vien

--- resources/views/khoa/qlgv.blade.php
+++ resources/views/khoa/qlgv.blade.php
@@ -1,5 +1,7 @@
 <div id="tabs-3">
-<label>Nhập tay: </label><button>Thêm giảng viên</button> <br />
+<label>Nhập tay: </label>
+<button class="btn btn-primary" data-toggle="modal" data-target="#modalthemgv">Thêm tay</button> 
+<br />
 <form id="form-upload" method="post" action="upload" onsubmit="return validateUploadForm(this)" enctype="multipart/form-data">
     {{ csrf_field() }}
 
@@ -12,6 +14,51 @@
 
 <div id="danhsachgiangvien">
 @include('khoa.danhsachgiangvien')
+</div>
+
+<!-- Modal Add Group -->
+<div class="modal fade" id="modalthemgv" role="dialog">
+    <div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+        	<div class="modal-header">
+	        	<button type="button" class="close" data-dismiss="modal">&times;</button>
+	        	<h2 class="modal-title">Thêm giảng viên</h2>
+	        </div>
+	        <div class="modal-body">
+	        	<form class="form" id="themgiangvien" method="post" action="">
+					<div class="row">
+						<div class="col-sm-2 col-sm-offset-1">
+							<label class="detail-label">Mã giảng viên: </label>
+						</div>
+						<div class="col-sm-8">
+							<input class="form-control" type="text">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2 col-sm-offset-1">
+							<label class="detail-label">Tên giảng viên: </label>
+						</div>
+						<div class="col-sm-8">
+							<input class="form-control" type="text">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2 col-sm-offset-1">
+							<label class="detail-label">Email: </label>
+						</div>
+						<div class="col-sm-8">
+							<input class="form-control" type="text">
+						</div>
+					</div>
+
+				</form>
+	        </div>
+        	<div class="modal-footer">
+        		<button type="button" class="btn btn-primary">Chấp nhận</button>
+        		<button type="button" class="btn btn-primary" data-dismiss="modal">Hủy</button>
+        	</div>
+    	</div>
+    </div>
 </div>
 
 </div>
