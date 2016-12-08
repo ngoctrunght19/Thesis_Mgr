0e1d8e3fb99cd558050880cce4fffb24e9ef9d47
Tran Minh Quan <tmq1201@gmail.com>
2016-12-08 09:40:59 +0700

    Chuyển cây Đơn vị vào view to + sửa lặt vặt :v

--- resources/views/hocvien.blade.php
+++ resources/views/hocvien.blade.php
@@ -19,35 +19,10 @@
               <a href="#tabs-1">
                   <div data-toggle="collapse" href="#item-khoa">
                     <svg class="glyph stroked notepad "><use xlink:href="#stroked-notepad"/></svg>
-                    Khoa
+                    Đơn vị
                   </div>
               </a>
-              <ul class="children collapse" id="item-khoa">
-                @foreach ($khoa as $khoa)
-                  <li>
-                    <a data-toggle="collapse" href="#item-{{ $khoa->makhoa }}">{{ $khoa->tenkhoa }}</a>
-                    <ul class="children collapse deeper" id="item-{{ $khoa->makhoa }}">
-                      <li>
-                        <a data-toggle="collapse" href="#item-{{ $khoa->makhoa+100 }}"> Bộ môn </a>
-                          <ul class="children collapse deeper" id="item-{{ $khoa->makhoa+100 }}">
-                            <li>
-                                <a href="#"> Phòng thí nghiệm </a>
-                            </li>
-                            <li>
-                                <a href="#"> Phòng thí nghiệm </a>
-                            </li>
-                          </ul>
-                      </li>
-                      <li>
-                        <a href="#"> Bộ môn </a>
-                      </li>
-                      <li>
-                        <a href="#"> Bộ môn </a>
-                      </li>
-                    </ul>
-                  </li>
-                @endforeach
-              </ul>
+
             </li>
             <li>
                 <a href="#tabs-2">
@@ -75,7 +50,7 @@
     <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
         @include('hocvien.donvi')
         @include('hocvien.giangvien', ['giangvien' => $giangvien, 'khoa' => $khoa, 'cdnc' => $cdnc])
-        @include('hocvien.linhvuc')
+        @include('hocvien.linhvuc') 
         <!-- @include('khoa.qlgv')
         @include('khoa.qlhv') -->
     </div>
--- resources/views/hocvien/linhvuc.blade.php
+++ resources/views/hocvien/linhvuc.blade.php
@@ -1,27 +1,19 @@
 <div id="tabs-3">
-	<div class="row">
-			<div class="col-lg-12">
-				<div class="panel panel-default">
-					<h1>Lĩnh vực & Chủ đề nghiên cứu</h1>
-					<div class="panel-body">
-            <ul class="nav menu" id="item-linhvuc">
-              @foreach ($linhvuc as $l)
-              <li class="">
-                <a data-toggle="collapse" href="#item-{{ $l->id+1000 }}">{{ $l->tenlinhvuc }}</a>
-                <ul class="nav  collapse deeper" id="item-{{ $l->id+1000 }}">
-                  @foreach ($cdnc as $c)
-                    @if($c->linhvuc == $l->id)
-                      <li class="">
-                        <a href="#">{{ $c->tenchude }}</a>
-                      </li>
-                    @endif
-                  @endforeach
-                </ul>
-              </li>
-              @endforeach
-            </ul>
-					</div>
-				</div>
-			</div>
-		</div><!--/.row-->
+	<h1>Lĩnh vực & Chủ đề nghiên cứu</h1>
+  <ul class="nav menu" id="item-linhvuc">
+    @foreach ($linhvuc as $l)
+    <li class="">
+      <a data-toggle="collapse" href="#item-{{ $l->id+1000 }}">{{ $l->tenlinhvuc }}</a>
+      <ul class="nav collapse deeper" id="item-{{ $l->id+1000 }}">
+        @foreach ($cdnc as $c)
+          @if($c->linhvuc == $l->id)
+            <li class="">
+              <a href="#">{{ $c->tenchude }}</a>
+            </li>
+          @endif
+        @endforeach
+      </ul>
+    </li>
+    @endforeach
+  </ul>
 </div>
