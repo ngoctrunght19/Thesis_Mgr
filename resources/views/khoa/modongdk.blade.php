ce638f05605bcdca158e1ff6284210aa568fdbf4
ngoctrunght19 <ngoctrunght19@gmail.com>
2016-12-14 22:25:34 +0700

    mở đóng đăng ký theo khoa

--- resources/views/khoa/modongdk.blade.php
+++ resources/views/khoa/modongdk.blade.php
@@ -3,7 +3,7 @@
 @section('tab-view')
 <h2>Mở và đóng đăng ký đề tài</h2>
 <div>
-	<button class="btn btn-primary">Mở đăng ký</button>
+	<button id="open-close" class="btn btn-primary"><span>{{ $hanhdong }}</span></button>
 </div>
 
 @endsection
